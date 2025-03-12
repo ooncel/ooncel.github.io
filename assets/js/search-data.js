6ac3941fc6f8c31716f3ed70300922b6a3cff862
ooncel <ogul.oncel@proton.me>
2025-03-12 13:04:12 +0000

    Deploying to gh-pages from @ ooncel/ooncel.github.io@727cee164c371a27847c69fd5e33f228b0d458c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,14 +94,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-the-blog-is-now-online-godspeed-smile",
           title: 'The blog is now online. Godspeed! :smile:',
           description: "",
